c5a86fa97704dd145d87b7f874fc097144d205fc
Evan Purcer <ep@e2.gg>
2026-08-16 13:47:18 -0700

    reorder PostCSS plugins to ensure nesting and autoprefixer operate last

--- assets/postcss.config.cjs
+++ assets/postcss.config.cjs
@@ -8,8 +8,8 @@ const path = require("path")
 module.exports = {
   plugins: [
     require("postcss-import")({ path: process.env.NODE_PATH.split(path.delimiter) }),
+    require("@tailwindcss/postcss"),
     require("postcss-nesting"),
     require("autoprefixer"),
-    require("@tailwindcss/postcss"),
   ],
 }
